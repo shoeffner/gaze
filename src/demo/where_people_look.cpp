809bc014c648515a99ee09663c3e1acec0c3553f
Sebastian Höffner <info@sebastian-hoeffner.de>
2017-09-13 20:27:35 +0200

    Searching only libraries which are needed for each build

---
+++ src/demo/where_people_look.cpp
@@ -0,0 +1,18 @@
+// Copyright 2017 Sebastian Höffner
+// This is a reimplementation of the data gathering experiment described in
+// section 2.1 of Judd et al.: Learning to Predict Where Humans Look (2009)
+//
+// @InProceedings{Judd_2009,
+//   author    = {Tilke Judd and Krista Ehinger and Fr{\'e}do Durand and Antonio Torralba},
+//   title     = {Learning to Predict Where Humans Look},
+//   booktitle = {IEEE International Conference on Computer Vision (ICCV)},
+//   year      = {2009}
+// }
+//
+// Checkout their fantastic website, showing off their results:
+// http://people.csail.mit.edu/tjudd/WherePeopleLook/interactiveWebsite/seeFixations.html
+
+#include "gaze/gaze.h"
+
+int main(int argc, const char** argv) {
+}

697cb3d0bf4d22174a990743deaad9205c70b5e6
Sebastian Höffner <info@sebastian-hoeffner.de>
2017-09-19 17:35:58 +0200

    Properly applying CSS Styles to GTK Widgets

--- src/demo/where_people_look/where_people_look.cpp
+++ src/demo/where_people_look/where_people_look.cpp
@@ -39,6 +39,15 @@ static const void cb_key_press(const GtkWidget* widget,
   }
 }
 
+static const void set_css_style(GtkWidget* window, const char* css_resource) {
+  GtkStyleContext* style_context = gtk_widget_get_style_context(window);
+  GtkCssProvider* style_provider = gtk_css_provider_new();
+  gtk_css_provider_load_from_resource(style_provider, css_resource);
+  gtk_style_context_add_provider(style_context,
+      GTK_STYLE_PROVIDER(style_provider),
+      GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+}
+
 static bool cb_swap_image(GtkWidget* image) {
   gtk_image_set_from_file(GTK_IMAGE(image),
       "./wpl_stimuli/syntheticData1.jpeg");
@@ -62,14 +71,16 @@ int main(int argc, char** argv) {
   gtk_init(&argc, &argv);
 
   GtkBuilder* builder = gtk_builder_new_from_resource("/wpl/where_people_look.ui");  // NOLINT
-  ::gui::register_and_connect_callbacks(builder);
+  GtkWidget* window = GTK_WIDGET(gtk_builder_get_object(builder, "MainWindow"));
+  GtkWidget* image = GTK_WIDGET(gtk_builder_get_object(builder, "Image"));
 
-  GObject* window = gtk_builder_get_object(builder, "MainWindow");
-  gtk_window_present(GTK_WINDOW(window));
+  ::gui::set_css_style(GTK_WIDGET(window), "/wpl/where_people_look.css");
+  ::gui::register_and_connect_callbacks(builder);
 
   g_object_unref(builder);
 
-  // Start GUI loop
+  gtk_window_fullscreen(GTK_WINDOW(window));
+
   gtk_main();
 
   return 0;

f3ba10f604cec5b90eadb2f71dfc40f2b5e65539
Sebastian Höffner <info@sebastian-hoeffner.de>
2017-10-03 20:41:39 +0200

    Adjusting wpl namespace documentation

--- src/demo/where_people_look/where_people_look.cpp
+++ src/demo/where_people_look/where_people_look.cpp
@@ -1,17 +1,15 @@
 // Copyright 2017 Sebastian Höffner
-// This is a crude reimplementation of the data gathering experiment described
-// in section 2.1 of Judd et al.: Learning to Predict Where Humans Look (2009)
-//
-// @InProceedings{Judd_2009,
-//   author    = {Tilke Judd and Krista Ehinger and Fr{\'e}do Durand and
-//                Antonio Torralba},
-//   title     = {Learning to Predict Where Humans Look},
-//   booktitle = {IEEE International Conference on Computer Vision (ICCV)},
-//   year      = {2009}
-// }
-//
-// Check out their fantastic website, showing off their results:
-// http://people.csail.mit.edu/tjudd/WherePeopleLook/interactiveWebsite/seeFixations.html
+/**
+ * @namespace wpl
+ * @brief Data acquisition after Judd et al. (2009) \cite judd2009
+ *
+ * This is a reimplementation of the data acquisition experiment described in
+ * section 2.1 of Judd et al.: Learning to Predict Where Humans Look (2009)
+ * \cite judd2009.
+ *
+ * Check out their fantastic website, showing off their results:
+ * <a href="http://people.csail.mit.edu/tjudd/WherePeopleLook/interactiveWebsite/seeFixations.html">Interactive fixations</a>
+ */
 
 #include "gaze/gaze.h"
 #include "gtk/gtk.h"

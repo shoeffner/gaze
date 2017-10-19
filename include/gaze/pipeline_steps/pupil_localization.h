472b58d960bc5c1fdbd9d07c64426ddcefc8cd6c
Sebastian Höffner <info@sebastian-hoeffner.de>
2017-10-19 21:25:25 +0200

    Adding displacement lookup table generation.

--- include/gaze/pipeline_steps/pupil_localization.h
+++ include/gaze/pipeline_steps/pupil_localization.h
@@ -12,14 +12,69 @@ namespace gaze {
 namespace pipeline {
 
 /**
- * @class PupilLocalization pupil_localization.h
- *        "gaze/pipeline_steps/pupil_localization.h"
+ * @class PupilLocalization pupil_localization.h "gaze/pipeline_steps/pupil_localization.h"
  * @brief Implements Timm and Barth (2011) @cite timm2011 to detect eye
  *        centers.
  *
  * // TODO(shoeffner): Write documentation for PupilLocalization.
  */
 class PupilLocalization final : public PipelineStep {
+  dlib::matrix<double> displacement_table_x;
+  dlib::matrix<double> displacement_table_y;
+
+  // TODO(shoeffner): Add unit tests.
+
+  /**
+   * Filles the displacement table to precalculate values for vector @f$d_i@f$
+   * @cite timm2011.
+   *
+   * The function returns without modifying table_x or table_y if size is
+   * smaller than the current table size.
+   *
+   * The size will always be set to size or size + 1, whichever is odd. This
+   * ensures that the center value is 0 as a reference point.
+   *
+   * After a call to this function with a sufficiently big size argument,
+   * table_x and table_y contain values such that reading from both tables
+   * at the same indices yields the respective components for a unit vector
+   * geometrically pointing from the center of the matrix to the read position.
+   *
+   * A @f$3 \times 3@f$ table_x would thus look like this:
+   *
+   * @f[
+   * \left(
+   * \begin{array}{ccc}
+   * -0.707 & 0 & 0.707 \\
+   * -1 & 0 & 1 \\
+   * -0.707 & 0 & 0.707
+   * \end{array}
+   * \right)
+   * @f]
+   *
+   * And the corresponding @f$3 \times 3@f$ table_y would be table_x's transposed
+   * version:
+   *
+   * @f[
+   * \left(
+   * \begin{array}{ccc}
+   * -0.707 & -1 & -0.707 \\
+   * 0 & 0 & 0 \\
+   * 0.707 & 1 & 0.707
+   * \end{array}
+   * \right)
+   * @f]
+   *
+   * Accessing table_x and table_y at (0, 0) yields @f$(-0.707, -0.707)@f$ which
+   * is a unit length vector pointing from the center (1, 1) to (0, 0).
+   *
+   * @param table_x The x components of @f$d_i@f$.
+   * @param table_y The y components of @f$d_i@f$.
+   * @param size The size to grow this table to.
+   */
+  void fill_displacement_tables(dlib::matrix<double>& table_x,
+                                dlib::matrix<double>& table_y,
+                                int size);
+
  public:
     /**
      */
--- src/gaze/pipeline_steps/pupil_localization.cpp
+++ src/gaze/pipeline_steps/pupil_localization.cpp
@@ -2,6 +2,8 @@
 
 #include "gaze/pipeline_steps/pupil_localization.h"
 
+#include <algorithm>
+#include <limits>
 #include <vector>
 
 #include "dlib/image_processing.h"
@@ -17,8 +19,52 @@ namespace gaze {
 
 namespace pipeline {
 
+void PupilLocalization::fill_displacement_tables(
+    dlib::matrix<double>& table_x,
+    dlib::matrix<double>& table_y,
+    int size) {
+  // Ensure size is bigger than the current size (i.e. if an update is needed
+  // at all)
+  if (size <= table_x.nr()) {
+    return;
+  }
+
+  // Ensure size is odd
+  size += (1 - size & 1);
+  int half_size = (size - 1) / 2;
+
+  // Resize tables
+  table_x.set_size(size, size);
+  table_y.set_size(size, size);
+
+  // Fill with indices
+  for (int row = 0; row < table_x.nr(); ++row) {
+    dlib::set_rowm(table_x, row) = dlib::range(-half_size, half_size);
+  }
+  table_y = dlib::trans(table_x);
+
+  // Calculate norms and store to table_x, then transpose to table_y
+  const double epsilon = std::numeric_limits<double>::min();
+  for (int row = 0; row < table_x.nr(); ++row) {
+    for (int col = 0; col < table_x.nc(); ++col) {
+      table_x(row, col) = table_x(row, col) /
+        (std::sqrt(table_x(row, col) * table_x(row, col)
+                   + table_y(row, col) * table_y(row, col))
+         // Avoid div0
+         + epsilon);
+    }
+  }
+  table_y = dlib::trans(table_x);
+}
+
 PupilLocalization::PupilLocalization() {
   this->name = "Eye center";
+  // Pre-calculate displacement table
+  // TODO(shoeffner): Evaluate which size is useful
+  this->fill_displacement_tables(
+      this->displacement_table_x,
+      this->displacement_table_y,
+      131);
 }
 
 void PupilLocalization::process(util::Data& data) {
@@ -29,6 +75,15 @@ void PupilLocalization::process(util::Data& data) {
   std::vector<dlib::chip_details> details =
     util::get_eyes_chip_details(data.landmarks);
   dlib::extract_image_chips(data.image, details, data.eyes);
+  int max_size = std::max({data.eyes[0].nr(), data.eyes[0].nc(),
+                           data.eyes[1].nr(), data.eyes[1].nc()});
+  if (2 * max_size > this->displacement_table_x.nr()) {
+    // TODO(shoeffner): Instead of +1 use +N to prepare for some more variation
+    this->fill_displacement_tables(
+        this->displacement_table_x,
+        this->displacement_table_y,
+        2 * max_size + 1);
+  }
 }
 
 void PupilLocalization::visualize(util::Data& data) {
@@ -54,7 +109,6 @@ void PupilLocalization::visualize(util::Data& data) {
                     data.centers[i] - offset_y,
                     dlib::rgb_pixel(255, 0, 0));
   }
-
 
   this->widget->set_image(dlib::tile_images(eyes));
 }

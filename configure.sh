03f2b23c157e0db5bd5ef63f2a033a9cd6261820
Sebastian Höffner <info@sebastian-hoeffner.de>
2017-11-14 13:39:42 +0100

    Adding dryrun option to configure.sh

--- configure.sh
+++ configure.sh
@@ -9,9 +9,13 @@
 #   --examples   Configures the examples to be built.
 #   --docs       Configures the documentation to be built.
 #   --tagfiles   Configures the documentation to link to external projects.
+#   --dry        Only prints the generated CMake command for custom
+#                modifications.
 #
 # You may want to override the environment variable BUILD_DIR to use another
 # directory than `build` for the build process.
+# Similarly you can override INSTALL_DIR to change the cmake install prefix.
+# --develop defaults the INSTALL_DIR to the build dir.
 
 BUILD_DIR=${BUILD_DIR:=build}
 
@@ -28,9 +32,9 @@ for arg in $*; do
             ;;
         "--develop")
             echo "Adding development tools. Building basic docs."
-            BUILD_CTAGS=ON
-            EXPORT_COMPILE_COMMANDS=ON
             DEPENDENCY_GRAPH=1
+            EXPORT_COMPILE_COMMANDS=ON
+            INSTALL_DIR=${INSTALL_DIR:=${BUILD_DIR}}
             ;;
         "--examples")
             echo "Building examples."
@@ -44,6 +48,10 @@ for arg in $*; do
             echo "Linking to external tagfiles."
             DOXYGEN_TAGFILES=ON
             ;;
+        "--dry" | "--dryrun" | "--dry-run")
+            echo "Just printing command."
+            DRYRUN=1
+            ;;
     esac
 done
 
@@ -52,33 +60,44 @@ BUILD_CTAGS=${BUILD_CTAGS:=OFF}
 BUILD_DOCS=${BUILD_DOCS:=OFF}
 BUILD_EXAMPLES=${BUILD_EXAMPLES:=OFF}
 CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:=Release}
+DEPENDENCY_GRAPH=${DEPENDENCY_GRAPH:=0}
 DOXYGEN_TAGFILES=${DOXYGEN_TAGFILES:=OFF}
 EXPORT_COMPILE_COMMANDS=${EXPORT_COMPILE_COMMANDS:=OFF}
 VERBOSE_MAKEFILE=${VERBOSE_MAKEFILE:=OFF}
-DEPENDENCY_GRAPH=${DEPENDENCY_GRAPH:=0}
+
+INSTALL_DIR=${INSTALL_DIR:=/usr/local}
+
+DRYRUN=${DRYRUN:=0}
 
 # Make directory if needed and run cmake
-mkdir -p ${BUILD_DIR}
+mkdir -p "${BUILD_DIR}"
 (
     PROJECT_DIR=$(pwd)
-    cd ${BUILD_DIR}
-    cmake -DBUILD_CTAGS=${BUILD_CTAGS} \
-          -DBUILD_DOCS=${BUILD_DOCS} \
-          -DBUILD_EXAMPLES=${BUILD_EXAMPLES} \
-          -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
-          `[[ "${BUILD_DOCS}" == "ON" ]] && echo "-DDOXYGEN_TAGFILES=${DOXYGEN_TAGFILES}"` \
-          -DCMAKE_EXPORT_COMPILE_COMMANDS=${EXPORT_COMPILE_COMMANDS} \
-          -DCMAKE_VERBOSE_MAKEFILE=${VERBOSE_MAKEFILE} \
-          `[[ ${DEPENDENCY_GRAPH} -eq 0 ]] || echo --graphviz=dependencies/dependencies.dot` \
+    cd "${BUILD_DIR}"
+    command="
+    cmake -DBUILD_CTAGS=${BUILD_CTAGS}
+          -DBUILD_DOCS=${BUILD_DOCS}
+          -DBUILD_EXAMPLES=${BUILD_EXAMPLES}
+          -DCMAKE_BUILD_TYPE=${BUILD_TYPE}
+          `[[ "${BUILD_DOCS}" == "ON" ]] && echo "-DDOXYGEN_TAGFILES=${DOXYGEN_TAGFILES}"`
+          -DCMAKE_EXPORT_COMPILE_COMMANDS=${EXPORT_COMPILE_COMMANDS}
+          -DCMAKE_VERBOSE_MAKEFILE=${VERBOSE_MAKEFILE}
+          `[[ ${DEPENDENCY_GRAPH} -eq 0 ]] || echo --graphviz=dependencies/dependencies.dot`
+          -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}
           ${PROJECT_DIR}
-    if [[ $? -eq 0 ]]; then
-        echo -e "\n\tYou can cd into ${BUILD_DIR} and call \"make -j8\" there:\n"
-        echo -e "\tcd ${BUILD_DIR}\n\tmake -j8\n"
-    else
-        echo -e "\n\tFailure!\n"
-        return 1
+    "
+    echo $command
+    if [[ ${DRYRUN} -eq 0 ]]; then
+        exec $command
+        if [[ $? -eq 0 ]]; then
+            echo -e "\n\tYou can go into ${BUILD_DIR} and compile and install from there:\n"
+            echo -e "\tcd ${BUILD_DIR}\n\tmake -j8\n\tmake install\n"
+        else
+            echo -e "\n\tFailure!\n"
+            return 1
+        fi
     fi
 )
-if [[ ${DEPENDENCY_GRAPH} -ne 0 && -f ${BUILD_DIR}/dependencies/dependencies.dot ]]; then
-    dot -Tpdf ${BUILD_DIR}/dependencies/dependencies.dot -o ${BUILD_DIR}/dependencies.pdf
+if [[ ${DEPENDENCY_GRAPH} -ne 0 && -f "${BUILD_DIR}/dependencies/dependencies.dot" && ${DRYRUN} -ne 0 ]]; then
+    dot -Tpdf "${BUILD_DIR}/dependencies/dependencies.dot" -o "${BUILD_DIR}/dependencies.pdf"
 fi

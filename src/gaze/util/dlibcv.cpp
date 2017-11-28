b2801e4f12fc6e752fbaab9a2492c0f89f14a99e
Sebastian Höffner <info@sebastian-hoeffner.de>
2017-11-28 22:44:46 +0100

    Adding utility OpenCV/dlib conversion functions.

---
+++ src/gaze/util/dlibcv.cpp
@@ -0,0 +1,26 @@
+// Copyright 2017 Sebastian Höffner
+
+#include "gaze/util/dlibcv.h"
+
+
+namespace gaze {
+
+namespace util {
+
+dlib::point cv_to_dlib(cv::Point to_convert) {
+  return {to_convert.x, to_convert.y};
+}
+
+dlib::rectangle cv_to_dlib(cv::Rect to_convert) {
+  return {to_convert.x, to_convert.y,
+          to_convert.width, to_convert.height};
+}
+
+cv::Rect dlib_to_cv(dlib::rectangle to_convert) {
+  return cv::Rect(to_convert.left(), to_convert.top(),
+                  to_convert.width(), to_convert.height());
+}
+
+}  // namespace util
+
+}  // namespace gaze
